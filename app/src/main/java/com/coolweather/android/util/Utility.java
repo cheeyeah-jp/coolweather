65fbfdfcadf66fdedd159cb7a041d9af7ee9322a
cheeyeah-jp <462367075@qq.com>
2020-03-06 11:09:37 +0800

    First Commit.

---
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -0,0 +1,36 @@
+package com.coolweather.android.util;
+
+import android.text.TextUtils;
+
+import com.coolweather.android.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Utility {
+
+
+    public static boolean handleProvinceResponse(String response) {
+        if (!TextUtils.isEmpty(response)) {
+
+            try {
+                JSONArray allProvinces = new JSONArray(response);
+
+                for (int i = 0; i < allProvinces.length(); i++) {
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+
+}
